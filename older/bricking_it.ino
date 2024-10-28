8a0b6e2354bc7ed8c5477cf0b907ad7a7da95961
Seán O'Sullivan <seangerardosullivan@gmail.com>
2024-10-28 12:02:07 +0000

    restrucutred project files

---
+++ older/bricking_it.ino
@@ -0,0 +1,11 @@
+// the moment when bricked the rp2040
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(1000);                     
+}
